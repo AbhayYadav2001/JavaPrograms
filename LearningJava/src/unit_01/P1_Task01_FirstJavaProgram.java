fe2899d76a4d757ff980130d753a648399d723a7
hp <hp@ABHAY-YADAV>
2022-03-11 16:03:00 +0530

    1st Commit

---
+++ LearningJava/src/unit_01/P1_Task01_FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package unit_01;
+
+public class P1_Task01_FirstJavaProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HELLO WORLD");
+
+	}
+
+}
